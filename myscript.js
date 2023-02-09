a48ef3e11de994da81eecbf3d47b05bb0974893b
alihamdan2020 <ali_hamdane@hotmail.com>
2023-02-09 16:38:26 +0200

    add new if condition in SASS and review for js change color dynamically

--- myscript.js
+++ myscript.js
@@ -2,4 +2,14 @@ var a =document.getElementById("burger");
 a.addEventListener("click",function() {
    var menu=document.getElementsByClassName("menue")[0];
    menu.style.display="flex";
-})   
+})  
+
+var colors=document.querySelectorAll(".color");
+colors.forEach(function(span){
+   span.addEventListener("click",function(){
+      if(span.getAttribute("data-color")=="red")
+         document.body.classList.add("color2");
+         else
+         document.body.classList.add("color3");
+   })
+})
